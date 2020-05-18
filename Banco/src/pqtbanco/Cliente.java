38081bfd7fea5e498bcab410fc8e5a6fbb6fee35
Ana <Ana@AnaPC.Home>
2020-05-18 23:23:19 +0200

    + calcularSaldoMedio

--- Banco/src/pqtbanco/Cliente.java
+++ Banco/src/pqtbanco/Cliente.java
@@ -7,7 +7,17 @@ public class Cliente {
 	private int nif;
 	private int edad;
 	private int total_gasto_tarjetas;
-
+	private int saldo_medio;
+	private int[] saldo_ultimos12m = new int[12];
 	
-
+	public int calcularSaldoMedio() {
+		int saldo_total=0;
+		
+		
+		for (int i = 0; i < saldo_ultimos12m.length; i++) {
+			saldo_total+= saldo_ultimos12m[i];
+		}
+		saldo_medio=saldo_total/12;
+		return saldo_medio;
+	}
 }
