4723ee97f2e7f57058b9a9e29b8bea87296a97c0
Ana <Ana@AnaPC.Home>
2020-05-21 14:26:42 +0200

    Control de calcularSaldoMedio

--- Banco/src/pqtbanco/Cliente.java
+++ Banco/src/pqtbanco/Cliente.java
@@ -8,58 +8,51 @@ public class Cliente {
 	private int edad;
 	private int saldo_medio;
 
+	public Cliente() {
 	
+	}
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getDireccion() {
 		return direccion;
 	}
-
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-
 	public String getApellido() {
 		return apellido;
 	}
-
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
 	public int getNif() {
 		return nif;
 	}
-
 	public void setNif(int nif) {
 		this.nif = nif;
 	}
-
 	public int getEdad() {
 		return edad;
 	}
-
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
 	public int getSaldo_medio() {
 		return saldo_medio;
 	}
-
-	public void setSaldo_medio(int saldo_medio) {
+	public void setSaldo_medio() {
 		this.saldo_medio = saldo_medio;
 	}
 
 	public int calcularSaldoMedio() {
 		int saldo_total=0;
-
+		saldo_total = (int) (10000*Math.random());
+		saldo_total = Math.round(saldo_total);
+		saldo_total = saldo_total*12;
 		saldo_medio=saldo_total/12;
 		return saldo_medio;
 	}
